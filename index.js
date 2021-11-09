b2ce75ac976742e06583c06b37551d6815d12f33
Abhishek Muralidharan <75883645+abhishek68@users.noreply.github.com>
2021-11-09 12:22:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app  = express();
 // app const contain express
 const PORT =process.env.PORT || 3000
 app.use(express.json());
+app.use(cors());
 const userModel = require('./user')
 // mongoose model
 
